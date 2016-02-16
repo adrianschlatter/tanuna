c7ac43abe45213735099a358a6232798f03bd690
Adrian Schlatter <schlatter@phys.ethz.ch>
2016-02-16 18:26:58 +0100

    pyDynamics removed ++++++++++++++++++
    `grep -R "pyDynamics" *` turned up some remaining pyDynamics references.
    Changed those to 'tanuna'.

--- docs/make.bat
+++ docs/make.bat
@@ -127,9 +127,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\pyDynamics.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\tanuna.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\pyDynamics.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\tanuna.ghc
 	goto end
 )
 
